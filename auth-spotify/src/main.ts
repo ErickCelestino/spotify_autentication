101aadb4e7af71f49f2c26df6768bc2aeae5d28a
ErickCelestino <erickcelestimo@gmail.com>
2023-05-14 22:13:37 -0300

    Criando estrutura basica

--- auth-spotify/src/main.ts
+++ auth-spotify/src/main.ts
@@ -3,13 +3,12 @@ import { redirecToAuthCodeFlow,  getAccessToken, populateUI, fetchProfile } from
 
 const clientId = '5e91022d605b43d5840f0648348e9348';
 const params = new URLSearchParams(window.location.search);
-const code = params.get("code")
+let code = params.get("code")
 
 if(!code) {
-  console.log(redirecToAuthCodeFlow(clientId))
+  redirecToAuthCodeFlow(clientId)
 } else {
   const acessToken = await getAccessToken(clientId, code)
   const profile = await fetchProfile(acessToken)
   populateUI(profile)
 }
-
